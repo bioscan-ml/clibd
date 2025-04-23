0ca889814a97a30701603786766127246ba7c501
zmgong <ming2280089874@gmail.com>
2025-04-23 08:47:23 -0700

    None leadd to error during collecting, replace none with empty list

--- bioscanclip/util/dataset.py
+++ bioscanclip/util/dataset.py
@@ -290,9 +290,9 @@ class Dataset_for_CL(Dataset):
                 language_token_type_ids = torch.tensor(language_token_type_ids[0])
                 language_attention_mask = torch.tensor(language_attention_mask[0])
             else:
-                language_input_ids = None
-                language_token_type_ids = None
-                language_attention_mask = None
+                language_input_ids = []
+                language_token_type_ids = []
+                language_attention_mask = []
 
             # language_input_ids = self.hdf5_split_group["language_tokens_input_ids"][idx]
             # language_token_type_ids = self.hdf5_split_group["language_tokens_token_type_ids"][idx]
